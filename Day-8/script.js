9bdec903efcc3df27e42d372257119acd93854ff
“SantaLauva” <santalauva089@gmail.com>
2022-08-10 16:45:41 +0300

    Day-8

---
+++ Day-8/script.js
@@ -0,0 +1,55 @@
+class ToDo {
+    constructor(task) {
+        this.id = -1;
+        this.done = false;
+        this.task_text = task;
+    }
+
+    createToDo() {
+        let toDo = document.querySelector('.to-do-tasks');
+        let template = document.querySelector('.to-do-template');
+        let clone = template.content.cloneNode(true);
+        clone.querySelector('.to-do-text').innerHTML = this.task_text;
+
+        tasks.push(this);
+        this.id = tasks.length - 1;
+        clone.querySelector('.to-do-checkbox').dataset.id = this.id;
+
+        toDo.appendChild(clone);
+    }
+}
+
+
+let tasks = [];
+
+document.getElementById('create-to-do').onclick = function(event) {
+    let inputField = document.getElementById('to-do-input');
+    let task = inputField.value;
+
+    let to_do = new ToDo(task);
+    to_do.createToDo();
+
+    inputField.value = "";
+}
+
+document.querySelector('.to-do-container').onclick = function(event) {
+    if (event.target.classList.contains('to-do-checkbox')) {
+        
+        tasks[event.target.dataset.id].done = true;
+        console.log(tasks[event.target.dataset.id].done);
+    }
+        
+}
+
+// function toDoDone() {
+//     let toDo = document.querySelector('.done-to-do');
+//     let template = document.querySelector('.done-template');
+//     let clone = template.content.cloneNode(true);
+//     clone.querySelector('.to-do-text').innerHTML = task;
+    
+//     toDo.appendChild(clone);
+// }
+
+// Jāsalabo toDoDone funkcija.
+//Vajag lai toDoDone padod taska datus
+
